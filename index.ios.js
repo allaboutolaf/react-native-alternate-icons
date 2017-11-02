7451fc8ea0a21e31d09c3cb0e1d2d35a22460d3f
Hawken Rives <rives@stolaf.edu>
2017-11-02 16:43:57 -0500

    rewrite the ios interface to use promises

---
+++ index.ios.js
@@ -0,0 +1,22 @@
+// @flow
+
+'use strict';
+
+import { NativeModules } from 'react-native';
+const AlternateIcons = NativeModules.AlternateIcons;
+
+export function setIconName(name: string): Promise<void> {
+    return AlternateIcons.setIconName(name);
+}
+
+export function reset(): Promise<void> {
+    return AlternateIcons.reset();
+}
+
+export function getIconName(): Promise<string> {
+    return AlternateIcons.getIconName();
+}
+
+export function isSupported(): Promise<boolean> {
+    return AlternateIcons.isSupported();
+}
