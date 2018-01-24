d2bb869eb62244be96cbacfb4274ea080c786431
Hawken Rives <hawkrives@gmail.com>
2018-01-24 15:41:18 -0600

    remove promise rejections on android

--- module.android.js
+++ module.android.js
@@ -4,12 +4,12 @@ export function setIconName(name: string): Promise<void> {
     return Promise.reject(new Error('Alternate icons are not supported on Android'));
 }
 
-export function reset(): Promise<void> {
-    return Promise.reject(new Error('Alternate icons are not supported on Android'));
+export function reset(): Promise<boolean> {
+    return Promise.resolve(false);
 }
 
 export function getIconName(): Promise<string> {
-    return Promise.reject(new Error('Alternate icons are not supported on Android'));
+    return Promise.resolve('default');
 }
 
 export function isSupported(): Promise<boolean> {
